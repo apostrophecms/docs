277649b567056196ada16bc378d362cfccb47ba4
Bob Means (BoDonkey) <robert@apostrophecms.com>
2023-09-22 15:33:29 -0400

    Add basic og to site

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -8,13 +8,14 @@ import AposTag from '../components/AposTag.vue'
 import AposRefExtends from '../components/AposRefExtends.vue'
 import AposFeedback from '../components/AposFeedback.vue'
 import EditOrIssue from '../components/EditOrIssue.vue'
+import AposOpenGraphInjector from '../components/AposOpenGraphInjector.vue'
 
 export default {
   ...Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'aside-outline-after': () => [ h(AposFeedback), h(EditOrIssue) ]
+      'aside-outline-after': () => [ h(AposFeedback), h(EditOrIssue), h(AposOpenGraphInjector) ]
     })
   },
   enhanceApp({ app, router, siteData}) {
