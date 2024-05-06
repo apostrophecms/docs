5cf1478311c92f1a2b761bbbee01b90d71bcd349
Stuart Romanek <stuart@apostrophecms.com>
2024-05-06 14:46:51 -0400

    sidebar and video styles

--- docs/.vitepress/components/AposVideoSidebar.vue
+++ docs/.vitepress/components/AposVideoSidebar.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="video-sidebar" v-if="hasVideos">
-    <ul>
-      <li v-for="video in videos" :key="video.id">
-        <a :href="video.link">
-          <span>{{ video.title }}</span>
-          <img :src="videoThumbnail(video.id)" :alt="video.title">
+  <section class="video-sidebar" v-if="hasVideos">
+    <h3 class="video-sidebar__header">Videos on this page</h3>
+    <ul class="video-sidebar__items">
+      <li class="video-sidebar__item" v-for="video in videos" :key="video.id">
+        <a class="video-sidebar__link" :href="video.link">
+          <p class="video-sidebar__title">{{ video.title }}</p>
+          <img class="video-sidebar__thumb"  :src="videoThumbnail(video.id)" :alt="video.title">
         </a>
       </li>
     </ul>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -42,3 +43,33 @@ export default {
   },
 };
 </script>
+
+<style lang="stylus" scoped>
+
+  .video-sidebar__header {
+    font-size: 14px;
+    font-weight: 600;
+    margin-bottom: 1rem;
+  }
+
+  .video-sidebar__items {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .video-sidebar__title {
+    font-size: 13px;
+    font-weight: 600;
+    margin-bottom: 0.15rem;
+  }
+
+  .video-sidebar__link:hover {
+    color: var(--vp-c-brand);
+
+    .video-sidebar__thumb {
+      outline: 4px solid var(--vp-c-brand);
+      outline-offset: -4px;
+    }
+  }
+</style>
