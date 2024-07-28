9e5c8734899c33931a148f752713471b5853c8ad
Robert Means <robert@robertmeans.net>
2024-07-28 08:20:20 -0400

    Creates note

---
+++ docs/.vitepress/components/AposAINote.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="note">
+    <p>
+      <strong>A note about generated documentation</strong><br>
+      Please note that the recommendations generated below are experimental and may not always be accurate. Please double-check the relevant parts of the official documentation before putting generated code into production.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AposAINote'
+};
+</script>
+
+<style scoped>
+.note {
+  background-color: #EEEEEE;
+  color: #444444;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #BCABF6;
+  border-radius: 8px;
+
+  p {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    margin-left: 20px;
+  }
+}
+
+</style>
